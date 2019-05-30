428c2eecaee7ac5857015498d23b74daa6882a81
Noah Stolk <noah.stolk1997@gmail.com>
2019-05-30 19:38:58 +0200

    no message

--- DevilDaggersSurvivalEditor/Properties/AssemblyInfo.cs
+++ DevilDaggersSurvivalEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DevilDaggersSurvivalEditor")]
-[assembly: AssemblyCopyright("Copyright © Noah Stolk 2018")]
+[assembly: AssemblyCopyright("Copyright © Noah Stolk 2018 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.5.1")]
-[assembly: AssemblyFileVersion("1.1.5.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
