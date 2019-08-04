db2892842474e542b4c47562ef8ba1a6dab33c72
Noah Stolk <noah.stolk1997@gmail.com>
2019-08-04 14:38:06 +0200

    Make sure only the OK button can close the Settings window.

--- DevilDaggersSurvivalEditor/GUI/Windows/Settings.xaml.cs
+++ DevilDaggersSurvivalEditor/GUI/Windows/Settings.xaml.cs
@@ -5,11 +5,22 @@ using System.Windows;
 using System.Diagnostics;
 using System.IO;
 using DevilDaggersSurvivalEditor.Code;
+using System.Runtime.InteropServices;
+using System;
+using System.Windows.Interop;
+using System.ComponentModel;
 
 namespace DevilDaggersSurvivalEditor.GUI.Windows
 {
 	public partial class SettingsWindow : Window
 	{
+		private const int GWL_STYLE = -16;
+		private const int WS_SYSMENU = 0x80000;
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+		[DllImport("user32.dll")]
+		private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
 		public SettingsWindow()
 		{
 			InitializeComponent();
@@ -19,6 +30,20 @@ namespace DevilDaggersSurvivalEditor.GUI.Windows
 			LabelSurvivalFileRootFolder.Content = UserHandler.Instance.settings.SurvivalFileRootFolder;
 
 			Data.DataContext = UserHandler.Instance.settings;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			// Remove Exit button
+			IntPtr hwnd = new WindowInteropHelper(this).Handle;
+			SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+		}
+
+		private void Window_Closing(object sender, CancelEventArgs e)
+		{
+			// Prevent Alt F4 from closing the window
+			if (!DialogResult.HasValue || !DialogResult.Value)
+				e.Cancel = true;
 		}
 
 		private void BrowseButton_Click(object sender, RoutedEventArgs e)
@@ -31,9 +56,7 @@ namespace DevilDaggersSurvivalEditor.GUI.Windows
 			CommonFileDialogResult result = dialog.ShowDialog();
 
 			if (result == CommonFileDialogResult.Ok)
-			{
 				SetSurvivalFileRootFolder(dialog.FileName);
-			}
 		}
 
 		private void AutoDetectButton_Click(object sender, RoutedEventArgs e)
