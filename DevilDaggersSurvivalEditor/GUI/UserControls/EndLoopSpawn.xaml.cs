a6f8bd55384fd3b37fa19a61006edf1c1e92f0f8
Noah Stolk <noah.stolk1997@gmail.com>
2019-08-05 14:55:15 +0200

    EndLoopSpawn user control code and layout improvements.

--- DevilDaggersSurvivalEditor/GUI/UserControls/EndLoopSpawn.xaml.cs
+++ DevilDaggersSurvivalEditor/GUI/UserControls/EndLoopSpawn.xaml.cs
@@ -64,33 +64,47 @@ namespace DevilDaggersSurvivalEditor.GUI.UserControls
 			{
 				enemy = value;
 
+				Color enemyColor = enemy == Spawnset.Enemies[-1] ? Color.FromRgb(0, 0, 0) : (Color)ColorConverter.ConvertFromString($"#{enemy.ToEnemy(GameInfo.GameVersions[GameInfo.DEFAULT_GAME_VERSION]).ColorCode}");
+				SolidColorBrush background = new SolidColorBrush(enemyColor);
+				SolidColorBrush foreground = new SolidColorBrush(MiscUtils.GetPerceivedBrightness(enemyColor) < 140 ? Color.FromRgb(255, 255, 255) : Color.FromRgb(0, 0, 0));
+
 				if (GigaBecomesGhost)
 				{
-					StackPanel stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
-					stackPanel.Children.Add(new Label { Content = enemy.Name, Margin = new Thickness(0), Padding = new Thickness(0) });
-					stackPanel.Children.Add(new Label { Content = "(?)", Margin = new Thickness(4, 0, 0, 0), Padding = new Thickness(0), FontWeight = FontWeights.Bold, ToolTip = "Every third wave of the end loop, all Gigapedes are changed into Ghostpedes. This is hardcoded within the game and cannot be changed." });
-					LabelEnemy.Content = stackPanel;
+					StackPanel stackPanel = new StackPanel
+					{
+						Orientation = Orientation.Horizontal,
+						Background = background
+					};
+					stackPanel.Children.Add(new Label
+					{
+						Content = enemy.Name,
+						Foreground = foreground
+					});
+					stackPanel.Children.Add(new Label
+					{
+						Content = "(?)",
+						FontWeight = FontWeights.Bold,
+						ToolTip = "Every third wave of the end loop, all Gigapedes are changed into Ghostpedes. This is hardcoded within the game and cannot be changed."
+					});
+					EnemyControl.Content = stackPanel;
 				}
 				else
 				{
-					LabelEnemy.Content = enemy.Name;
+					EnemyControl.Content = new Label
+					{
+						Content = enemy.Name,
+						Background = background,
+						Foreground = foreground
+					};
 				}
 
 				LabelNoFarmGems.Content = enemy.NoFarmGems;
-
-				Color color = enemy == Spawnset.Enemies[-1] ? Color.FromRgb(0, 0, 0) : (Color)ColorConverter.ConvertFromString($"#{enemy.ToEnemy(GameInfo.GameVersions[GameInfo.DEFAULT_GAME_VERSION]).ColorCode}");
-				LabelEnemy.Background = new SolidColorBrush(color);
-
-				if (MiscUtils.GetPerceivedBrightness(color) < 140)
-					LabelEnemy.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
 			}
 		}
 
 		public EndLoopSpawnUserControl()
 		{
 			InitializeComponent();
-
-			Background = new SolidColorBrush(Color.FromArgb(192, 255, 255, 255));
 		}
 	}
 }
