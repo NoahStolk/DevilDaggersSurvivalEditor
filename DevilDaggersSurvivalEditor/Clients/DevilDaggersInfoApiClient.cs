902cc02b67177f51650c7704b5ff1b483885b15a
Noah Stolk <noah.stolk1997@gmail.com>
2021-03-10 22:00:49 +0100

    Update API client

--- DevilDaggersSurvivalEditor/Clients/DevilDaggersInfoApiClient.cs
+++ DevilDaggersSurvivalEditor/Clients/DevilDaggersInfoApiClient.cs
@@ -2158,6 +2158,12 @@ namespace DevilDaggersSurvivalEditor.Clients
         [Newtonsoft.Json.JsonProperty("homingDaggersDiff", Required = Newtonsoft.Json.Required.Always)]
         public int HomingDaggersDiff { get; set; }= default!;
     
+        [Newtonsoft.Json.JsonProperty("homingDaggersEaten", Required = Newtonsoft.Json.Required.Always)]
+        public int HomingDaggersEaten { get; set; }= default!;
+    
+        [Newtonsoft.Json.JsonProperty("homingDaggersEatenDiff", Required = Newtonsoft.Json.Required.Always)]
+        public int HomingDaggersEatenDiff { get; set; }= default!;
+    
         [Newtonsoft.Json.JsonProperty("gemsDespawned", Required = Newtonsoft.Json.Required.Always)]
         public int GemsDespawned { get; set; }= default!;
     
@@ -2294,6 +2300,9 @@ namespace DevilDaggersSurvivalEditor.Clients
         [Newtonsoft.Json.JsonProperty("homingDaggers", Required = Newtonsoft.Json.Required.Always)]
         public int HomingDaggers { get; set; }= default!;
     
+        [Newtonsoft.Json.JsonProperty("homingDaggersEaten", Required = Newtonsoft.Json.Required.Always)]
+        public int HomingDaggersEaten { get; set; }= default!;
+    
         [Newtonsoft.Json.JsonProperty("gemsDespawned", Required = Newtonsoft.Json.Required.Always)]
         public int GemsDespawned { get; set; }= default!;
     
@@ -2388,6 +2397,9 @@ namespace DevilDaggersSurvivalEditor.Clients
     
         [Newtonsoft.Json.JsonProperty("homingDaggers", Required = Newtonsoft.Json.Required.Always)]
         public int HomingDaggers { get; set; }= default!;
+    
+        [Newtonsoft.Json.JsonProperty("homingDaggersEaten", Required = Newtonsoft.Json.Required.Always)]
+        public int HomingDaggersEaten { get; set; }= default!;
     
         [Newtonsoft.Json.JsonProperty("gemsDespawned", Required = Newtonsoft.Json.Required.Always)]
         public int GemsDespawned { get; set; }= default!;
