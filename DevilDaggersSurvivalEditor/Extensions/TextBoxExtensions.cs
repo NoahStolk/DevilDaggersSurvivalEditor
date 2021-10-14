7c9856bea4c8b64e3ddff6861bd7da215b2fe2a9
Noah Stolk <noah.stolk1997@gmail.com>
2021-10-14 17:41:09 +0200

    Refactor extensions and suppress suggestions

--- DevilDaggersSurvivalEditor/Extensions/TextBoxExtensions.cs
+++ DevilDaggersSurvivalEditor/Extensions/TextBoxExtensions.cs
@@ -1,27 +1,20 @@
 using DevilDaggersCore.Wpf.Utils;
+using System;
 using System.Windows.Controls;
 
 namespace DevilDaggersSurvivalEditor.Extensions
 {
 	public static class TextBoxExtensions
 	{
-		public static bool ValidatePositiveFloatTextBox(this TextBox textBox)
-		{
-			bool isValid = float.TryParse(textBox.Text, out float result) && result >= 0;
-			textBox.Background = isValid ? ColorUtils.ThemeColors["Gray2"] : ColorUtils.ThemeColors["ErrorBackground"];
-			return isValid;
-		}
+		public static bool ValidatePositiveFloatTextBox(this TextBox textBox) => Validate(textBox, (tb) => float.TryParse(tb.Text, out float result) && result >= 0);
 
-		public static bool ValidatePositiveIntTextBox(this TextBox textBox)
-		{
-			bool isValid = int.TryParse(textBox.Text, out int result) && result >= 0;
-			textBox.Background = isValid ? ColorUtils.ThemeColors["Gray2"] : ColorUtils.ThemeColors["ErrorBackground"];
-			return isValid;
-		}
+		public static bool ValidatePositiveIntTextBox(this TextBox textBox) => Validate(textBox, (tb) => int.TryParse(tb.Text, out int result) && result >= 0);
 
-		public static bool ValidateIntTextBox(this TextBox textBox)
+		public static bool ValidateIntTextBox(this TextBox textBox) => Validate(textBox, (tb) => int.TryParse(tb.Text, out _));
+
+		private static bool Validate(this TextBox textBox, Func<TextBox, bool> validator)
 		{
-			bool isValid = int.TryParse(textBox.Text, out _);
+			bool isValid = validator(textBox);
 			textBox.Background = isValid ? ColorUtils.ThemeColors["Gray2"] : ColorUtils.ThemeColors["ErrorBackground"];
 			return isValid;
 		}
