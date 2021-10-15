a7708e782d6a632f309af3c33773ff04666200e0
Noah Stolk <noah.stolk1997@gmail.com>
2021-10-15 11:54:54 +0200

    Color tiles higher than max blue

--- DevilDaggersSurvivalEditor/Utils/TileUtils.cs
+++ DevilDaggersSurvivalEditor/Utils/TileUtils.cs
@@ -23,6 +23,9 @@ namespace DevilDaggersSurvivalEditor.Utils
 			if (height < 0)
 				return Color.FromRgb((byte)(colorValue * (1 + Math.Abs(height * 0.5f))), (byte)(colorValue / 4), (byte)((height - TileMin) * 8));
 
+			if (height > TileMax)
+				return Color.FromRgb(0, 160, 255);
+
 			return Color.FromRgb((byte)colorValue, (byte)(colorValue / 2), (byte)((height - TileMin) * 4));
 		}
 	}
